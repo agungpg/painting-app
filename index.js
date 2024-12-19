1637868120416f752d49627fe89002d2ae9171b3
agungpg <perdanaagung71@gmail.com>
2024-12-19 13:03:30 +0800

    add styling and some assets

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 "use strict";
 var isPainting = false;
 var FillColor = "#000"
-var StrokeColor = "#000"
-const noStrokeColor = '#ffffff'
-function Circle(positionX, positionY, radius, fill, stroke, stroke_width) {
+var Pointer = "circle";
+function Circle(positionX, positionY, radius, fill) {
   this.positionX = positionX || 0;
   this.positionY = positionY || 0;
   this.radius = radius || 7;
   this.fill = fill || randColor();
-  if(noStrokeColor != stroke) {
-    this.stroke = stroke || randColor();
-    this.stroke_width = stroke_width || 2;
-  }
   this.draw = function () {
     JScontentCreation.innerHTML += `<circle class="circle" onclick="Erase(this)" cx="${this.positionX}" cy="${this.positionY}" 
-      r="${this.radius}" fill="${this.fill}" 
-      stroke="${this.stroke}" stroke-width="${this.stroke_width}"/>`;
+      r="${this.radius}" fill="${this.fill}" />`;
   };
 }
 function Squere(
@@ -24,30 +18,28 @@ function Squere(
   height,
   width,
   fill,
-  stroke,
-  strokeWidth
 ) {
   this.positionX = positionX || 0;
   this.positionY = positionY || 0;
   this.height = height;
   this.width = width;
   this.fill = fill;
-  if(noStrokeColor != stroke) {
-    this.stroke = stroke;
-    this.stroke_width = strokeWidth ?? 2;
-  }
   this.draw = function () {
-    JScontentCreation.innerHTML += `<rect class="squere" x="${this.positionX}" y="${this.positionY}" width="${this.width}" height="${height}" fill="${this.fill}" stroke="${this.stroke}" stroke-width="${this.stroke_width}" />`;
+    JScontentCreation.innerHTML += `<rect class="squere" x="${this.positionX}" y="${this.positionY}" width="${this.width}" height="${height}" fill="${this.fill}" />`;
   };
 }
-
-var Pointer = "circle";
 function randColor() {
   let r = Math.floor(Math.random() * 256),
     g = Math.floor(Math.random() * 256),
     b = Math.floor(Math.random() * 256);
   return "#" + r.toString(16) + g.toString(16) + b.toString(16);
 }
+
+function Erase(e) {
+  if (Pointer != "eraser") return;
+  e.remove();
+}
+
 window.addEventListener("mousedown", function () {
   isPainting = true;
 });
@@ -62,8 +54,8 @@ window.addEventListener(
     if (Pointer == "eraser") {
       for (let i = 0; i < JScontentCreation.children.length; i++) {
         const el = JScontentCreation.children[i];
-        const cx = el.attributes.cx.value;
-        const cy = el.attributes.cy.value;
+        const cx = (el.attributes.cx ?? el.attributes.x).value;
+        const cy = (el.attributes.cy ?? el.attributes.y).value;
         if (
           parseInt(cx) >= e.pageX - 8 &&
           parseInt(cx) <= e.pageX + 8 &&
@@ -78,8 +70,8 @@ window.addEventListener(
 
     let c1 =
       Pointer == "circle"
-        ? new Circle(e.pageX, e.pageY, 4, FillColor, StrokeColor)
-        : new Squere(e.pageX, e.pageY, 8, 8, FillColor, StrokeColor);
+        ? new Circle(e.pageX-4, e.pageY+10, 4, FillColor)
+        : new Squere(e.pageX-2, e.pageY+4, 8, 8, FillColor);
     c1.draw();
   },
   false
@@ -104,27 +96,23 @@ document.getElementById("exportBtn").addEventListener("click", () => {
 
 document.getElementById("squere").addEventListener("click", () => {
   Pointer = "squere";
+  JScontentCreation.style.cursor = 'url("paintbrush-2.png"),auto';
 });
 
 document.getElementById("circle").addEventListener("click", () => {
   Pointer = "circle";
+  JScontentCreation.style.cursor = 'url("paintbrush.png"),auto';
 });
 document.getElementById("eraser").addEventListener("click", () => {
   Pointer = "eraser";
+  JScontentCreation.style.cursor = 'url("eraser.png"), auto';
 });
 
-function Erase(e) {
-  if (Pointer != "eraser") return;
-  e.remove();
-}
 document.getElementById("reset").addEventListener("click", () => {
   JScontentCreation.innerHTML = "";
+  JScontentCreation.style.cursor = 'auto';
 });
 
 document.getElementById("color-picker").addEventListener("change", (e)=>{
     FillColor = e.target.value
-})
-
-document.getElementById("stroke-color-picker").addEventListener("change", (e)=>{
-    StrokeColor = e.target.value
 })
